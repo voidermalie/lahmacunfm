e09313510928501075fca6c42d3f8b63cf11cb5b
voidermalie <lestarfanny@gmail.com>
2024-03-28 14:54:08 +0100

    tabs style

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,40 +1,95 @@
 import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Tabs } from 'expo-router';
+import { LAHMACUN_PURPLE } from '../../util/constants';
+import { StyleSheet, View, Image } from 'react-native';
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: 'black',
+        tabBarStyle: styles.tabBarBackground
+      }}
+      sceneContainerStyle={styles.scrollView}>
       <Tabs.Screen
         name="home"
         options={{
-          headerShown: false,
           title: 'Radio',
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="home" color={color} />
-          )
+          tabBarIcon: RadioIcon
         }}
       />
       <Tabs.Screen
         name="shows/shows"
         options={{
           title: 'Shows',
-          headerShown: false,
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="cog" color={color} />
-          )
+          tabBarIcon: ShowsIcon
         }}
       />
       <Tabs.Screen
         name="donate"
         options={{
           title: 'Donate',
-          headerShown: false,
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="cog" color={color} />
-          )
+          tabBarIcon: DonateIcon
         }}
       />
     </Tabs>
   );
 }
+
+const RadioIcon = ({ focused }: { focused: boolean }) => (
+  <Image
+    style={{ ...styles.radioIcon, ...(!focused ? styles.inactive : '') }}
+    source={require('../../assets/img/lahmacun-logo.png')}
+  />
+);
+
+const ShowsIcon = ({ focused }: { focused: boolean }) => (
+  <Image
+    style={{ ...styles.showsIcon, ...(!focused ? styles.inactive : '') }}
+    source={require('../../assets/img/vinyl-record.png')}
+  />
+);
+
+const DonateIcon = ({ focused }: { focused: boolean }) => (
+  <Image
+    style={{ ...styles.donateIcon, ...(!focused ? styles.inactive : '') }}
+    source={require('../../assets/img/money.png')}
+  />
+);
+
+const styles = StyleSheet.create({
+  mainWrapper: {
+    paddingVertical: 15
+  },
+  scrollView: {
+    backgroundColor: LAHMACUN_PURPLE,
+    height: '100%',
+    display: 'flex',
+    alignContent: 'center',
+    justifyContent: 'center'
+  },
+  radioIcon: {
+    width: 30,
+    height: 25
+  },
+  showsIcon: {
+    width: 30,
+    height: 30
+  },
+  donateIcon: {
+    width: 30,
+    height: 30
+  },
+  inactive: {
+    tintColor: '#606060'
+  },
+  tabOptions: {
+    backgroundColor: LAHMACUN_PURPLE,
+    width: '100%',
+    height: '100%'
+  },
+  tabBarBackground: {
+    backgroundColor: LAHMACUN_PURPLE
+  }
+});
