83ebeeecebb4a276df6497c0bec3db756af1a6a9
voidermalie <lestarfanny@gmail.com>
2024-08-05 13:03:33 +0200

    firebase config

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,9 @@
 import { Redirect } from "expo-router";
+import { firebase, analytics } from '../firebase/firebaseConfig.js';
 
+// Log to verify initialization
+console.log('Firebase app initialized:', firebase.apps.length > 0);
+console.log('Firebase analytics initialized:', analytics != null);
 
 const Home = () => {
   return <Redirect href="/(tabs)/home"/>
--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -2,13 +2,18 @@ import firebase from '@react-native-firebase/app';
 import '@react-native-firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "AUTH_DOMAIN",
-  projectId: "PROJECT_ID",
-  storageBucket: "STORAGE_BUCKET",
-  messagingSenderId: "MESSAGING_SENDER_ID",
-  appId: "APP_ID",
-  measurementId: "MEASUREMENT_ID"
+  apiKey: process.env.FIREBASE_API_KEY,
+  gcmSenderId: process.env.FIREBASE_GCM_SENDER_ID,
+  plistVersion: "1",
+  bundleID: "com.lahmacun.lahmacunfm",
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  isAdsEnabled: "false",
+  isAnalyticsEnabled: "true",
+  isAppInviteEnabled: "true",
+  isGcmEnabled: "true",
+  isSigninEnabled: "false",
+  googleAppId: process.env.FIREBASE_GOOGLE_APP_ID
 };
 
 if (!firebase.apps.length) {
