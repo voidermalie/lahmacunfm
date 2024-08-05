a099d842df824a5bfadd2d72a0371180ce8c16f8
voidermalie <lestarfanny@gmail.com>
2024-08-05 18:47:39 +0200

    firebase init final fix

---
+++ app.config.js
@@ -0,0 +1,56 @@
+export default {
+      name: "lahmacunfm",
+      slug: "lahmacunfm",
+      scheme: "lahmacun",
+      version: "1.0.0",
+      orientation: "portrait",
+      icon: "./assets/logo/lahma120.png",
+      userInterfaceStyle: "light",
+      splash: {
+        image: "./assets/img/dj-citrom-web.png",
+        resizeMode: "contain",
+        backgroundColor: "#d09cf8"
+      },
+      assetBundlePatterns: [
+        "**/*"
+      ],
+      ios: {
+        supportsTablet: true,
+        bundleIdentifier: "com.lahmacun.lahmacunfm",
+        googleServicesFile: process.env.GoogleServiceInfoPlist
+      },
+      android: {
+        adaptiveIcon: {
+          foregroundImage: "./assets/adaptive-icon.png",
+          backgroundColor: "#d09cf8"
+        }
+      },
+      web: {
+        favicon: "./assets/favicon.png"
+      },
+      plugins: [
+        "expo-router",
+        "expo-font",
+        "@react-native-firebase/app",
+        [
+          "expo-build-properties",
+          {
+            ios: {
+              useFrameworks: "static"
+            }
+          }
+        ]
+      ],
+      experiments: {
+        typedRoutes: true
+      },
+      extra: {
+        router: {
+          origin: false
+        },
+        eas: {
+          projectId: "71202919-16bc-4e11-99ae-19794da03c00"
+        }
+      }
+    };
+  
--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -2,18 +2,18 @@ import firebase from '@react-native-firebase/app';
 import '@react-native-firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  gcmSenderId: process.env.FIREBASE_GCM_SENDER_ID,
+  apiKey: process.env.API_KEY,
+  gcmSenderId: process.env.GCM_SENDER_ID,
   plistVersion: "1",
   bundleID: "com.lahmacun.lahmacunfm",
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
   isAdsEnabled: "false",
   isAnalyticsEnabled: "true",
   isAppInviteEnabled: "true",
   isGcmEnabled: "true",
   isSigninEnabled: "false",
-  googleAppId: process.env.FIREBASE_GOOGLE_APP_ID
+  googleAppId: process.env.GOOGLE_APP_ID
 };
 
 if (!firebase.apps.length) {
